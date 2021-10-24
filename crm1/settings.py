37e9a2cc0518d559bee7b4ac61046e99f25bbe9a
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-24 16:28:20 +0300

    static root configuration

--- crm1/settings.py
+++ crm1/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
@@ -129,3 +130,4 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
