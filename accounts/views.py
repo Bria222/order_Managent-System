b8581461b8bd5e1386485ed4cf70afaf1ba1d231
Bria222 <briantechologies.co.ke@gmail.com>
2021-10-31 16:58:43 +0300

    adding user login and registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
 
+def registerPage(request):
+	form = UserCreationForm()
+	context = {'form':form}
+	return render(request,'accounts/register.html',context)
 
+def loginPage(request):
+	context = {}
+	return render(request,'accounts/login.html',context)
 
 def home(request):
 	orders = Order.objects.all()
